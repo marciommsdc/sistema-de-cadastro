0baedfc6d6f159af01b968448825e6b9be4ea9d6
marciommsdc <marciommsdc@gmail.com>
2023-02-10 20:30:48 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando com o bando de dados")
+// mais um console log de teste:
+console.log("O Paula fez esse commit sem você ver na aula!")
+console.log("Enviando para o Github")
